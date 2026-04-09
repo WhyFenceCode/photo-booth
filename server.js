aa4c1af2ccbca82a1971bb93a632c4eb12e68c20
WhyFenceCode <whyfencecode@gmail.com>
2026-04-09 13:52:25 -0700

    added startup ui, not yet tied in

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const sharp = require("sharp");
 const { exiftool } = require("exiftool-vendored");
 const { exec } = require("child_process");
 const readline = require("readline");
+const { styleText } = require('node:util');
 
 const HorizontalFRAME = {
   x: 0,
@@ -22,6 +23,7 @@ print = false;
 const { spawn } = require("child_process");
 const fs = require("fs");
 const path = require("path");
+const { start } = require("node:repl");
 
 const OUTPUT_DIR = path.join(__dirname, "temp");
 const PROCESSED_DIR = path.join(__dirname, "processed");
@@ -57,25 +59,92 @@ function askCopies() {
   });
 }
 
+function progressBar(step, total) {
+  const inside = "▒".repeat(step) + "⠀".repeat(total - step);
+  return `${inside}`;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function renderBar(before, current, color, spacer) {
+  const barLength = 50;
+
+  for (let i = 0; i <= barLength; i++) {
+    console.clear();
+    console.log(before);
+    console.log(current);
+    console.log("");
+
+    const bar = styleText(['black', 'bold', `${color}`], progressBar(i, barLength));
+    const barText = spacer + "[" + bar + "] " + i + "/" + barLength;
+    console.log(barText);
+
+    await sleep(10 + Math.random() * 20);
+  }
+}
+
+async function startUp() {
+  const spacerText = " ".repeat(10);
+  const smallSpacerText = " ".repeat(4);
+  const emptyLine = "";
+  const newLine = "\n";
+
+  const fullBar = "[✅]"
+
+  const coreText = styleText(['black', 'bold', 'bgGreen'], ' core ');
+  const cameraText = styleText(['black', 'bold', 'bgMagenta'], ' camera ');
+  const tempText = styleText(['black', 'bold', 'bgMagenta'], ' temp ');
+  const printText = styleText(['black', 'bold', 'bgMagenta'], ' print ');
+
+  const coreValueText = styleText(['black', 'bold'], 'Starting Launch Sequence');
+  const cameraValueText = styleText(['black', 'bold'], 'Loading Camera Integration');
+  const tempValueText = styleText(['black', 'bold'], 'Preparing Temp Storage');
+  const printValueText = styleText(['black', 'bold'], 'Connecting Printer');
+
+  const coreCompleteText = "\n" + spacerText + coreText + smallSpacerText + coreValueText + "\n\n" + spacerText + fullBar + "\n\n";
+  const cameraCompleteText = "\n" + spacerText + cameraText + smallSpacerText + cameraValueText + "\n\n" + spacerText + fullBar + "\n\n";
+  const tempCompleteText = "\n" + spacerText + tempText + smallSpacerText + tempValueText + "\n\n" + spacerText + fullBar + "\n\n";
+  const printCompleteText = "\n" + spacerText + printText + smallSpacerText + printValueText + "\n\n" + spacerText + fullBar + "\n\n";
+
+  console.clear();
+
+  await renderBar(emptyLine, spacerText + coreText + smallSpacerText + coreValueText, 'blue', spacerText);
+  await renderBar(coreCompleteText, spacerText + cameraText + smallSpacerText + cameraValueText, 'blue', spacerText);
+  await renderBar(coreCompleteText + cameraCompleteText, spacerText + tempText + smallSpacerText + tempValueText, 'blue', spacerText);
+  await renderBar(coreCompleteText + cameraCompleteText + tempCompleteText, spacerText + printText + smallSpacerText + printValueText, 'blue', spacerText);
+
+  console.clear();
+
+  console.log(coreCompleteText + cameraCompleteText + tempCompleteText + printCompleteText);
+  console.log("\n\n\n");
+}
+
 function startListener() {
-  const gp = spawn("gphoto2", ["--wait-event-and-download"]);
-  console.log("📡 Listening for photo...");
+  console.clear()
+  startUp()
+  .then(() => {
+    const gp = spawn("gphoto2", ["--wait-event-and-download"]);
 
-  gp.stdout.on("data", data => {
-    const text = data.toString();
-    //process.stdout.write(text);
+      gp.stdout.on("data", data => {
+      const text = data.toString();
+      //process.stdout.write(text);
 
-    const match = text.match(/Saving file as (.+\.(jpg|jpeg|JPG|JPEG))/);
-    if (match) {
-      const filename = match[1];
-      handleNewFile(filename);
-    }
-  });
+      const match = text.match(/Saving file as (.+\.(jpg|jpeg|JPG|JPEG))/);
+      if (match) {
+        const filename = match[1];
+        handleNewFile(filename);
+      }
+    });
 
-  gp.stderr.on("data", data => console.error("⚠️ gphoto2 error:", data.toString()));
-  gp.on("close", code => {
-    console.log(`❌ gphoto2 exited (${code}). Restarting...`);
-    setTimeout(startListener, 1000);
+    gp.stderr.on("data", data => {
+      console.error("⚠️ gphoto2 error:", data.toString());
+    });
+    gp.on("close", code => {
+      console.log(`❌ gphoto2 exited (${code}). Restarting...`);
+      setTimeout(startListener, 1000);
+    });
   });
 }
 
@@ -156,7 +225,7 @@ async function applyBorder(inputPath, outputPath) {
 
   const copies = await askCopies();
   if (print) await printPhoto(outputPath, copies);
-  console.log(" ");
+  console.clear()
   console.log("📡 Listening for photo...");
 }
 
